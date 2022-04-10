b16bf541a243940568ae6e9b78fd1c32f3d0f22b
Saulius Petreikis <saulius.petreikis@nfq.lt>
2022-04-10 11:36:07 +0300

    solutions: lesson-3

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,26 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema({
+    name: {
+        maxlength: 50, required: true, trim: true, type: String
+    },
+    status: {
+        type: String, required: true, enum: ['active', 'complete', 'pastdue'], default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    list: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'list',
+        required: true
+    }
+}, {timestamps: true})
+
+itemSchema.index({list: 1, name: 1}, {unique: true});
+
 export const Item = mongoose.model('item', itemSchema)
