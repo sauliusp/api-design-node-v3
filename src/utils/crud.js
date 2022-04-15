64d0e2f958544b6a149705784eab8f7f86084f42
Saulius Petreikis <saulius.petreikis@nfq.lt>
2022-04-15 09:15:35 +0300

    minor adjustments

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,59 +1,71 @@
 export const getOne = model => async (req, res) => {
-  const result = await model.findOne({
-    _id: req.params.id,
-    createdBy: req.user._id,
-  }).exec();
+  const result = await model
+    .findOne({
+      _id: req.params.id,
+      createdBy: req.user._id
+    })
+    .exec()
 
   if (!result) {
-    return res.status(404).end();
+    return res.status(404).end()
   }
 
-  res.status(200).json({data: result});
+  res.status(200).json({ data: result })
 }
 
 export const getMany = model => async (req, res) => {
-  const result = await model.find({
-    createdBy: req.user._id
-  }).exec()
+  const result = await model
+    .find({
+      createdBy: req.user._id
+    })
+    .exec()
 
-  res.status(200).json({data: result});
+  res.status(200).json({ data: result })
 }
 
 export const createOne = model => async (req, res) => {
   const result = await model.create({
     createdBy: req.user._id,
     ...req.body
-  });
+  })
 
-  res.status(201).json({data: result});
+  res.status(201).json({ data: result })
 }
 
 export const updateOne = model => async (req, res) => {
-  const result = await model.findOneAndUpdate({
-    _id: req.params.id,
-    createdBy: req.user._id
-  }, req.body, {
-    new: true
-  }).exec();
+  const result = await model
+    .findOneAndUpdate(
+      {
+        _id: req.params.id,
+        createdBy: req.user._id
+      },
+      req.body,
+      {
+        new: true
+      }
+    )
+    .exec()
 
   if (!result) {
-    return res.status(400).end();
+    return res.status(400).end()
   }
 
-  res.status(200).json({data: result});
+  res.status(200).json({ data: result })
 }
 
 export const removeOne = model => async (req, res) => {
-  const result = await model.findOneAndRemove({
-    _id: req.params.id,
-    createdBy: req.user._id
-  }).exec();
+  const result = await model
+    .findOneAndRemove({
+      _id: req.params.id,
+      createdBy: req.user._id
+    })
+    .exec()
 
   if (!result) {
-    return res.status(400).end();
+    return res.status(400).end()
   }
 
-  res.status(200).json({data: result});
+  res.status(200).json({ data: result })
 }
 
 export const crudControllers = model => ({
